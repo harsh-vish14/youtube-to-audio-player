b06a3e15dfbd518d4189ec8f2a0d2c16fd59ad56
harsh-vish14 <harshkv9322@gmail.com>
2021-01-12 15:40:36 +0530

    added new install

---
+++ public/sw.js
@@ -0,0 +1,53 @@
+const filesToCache = [
+  '/',
+  '/404',
+  '/playList',
+  'css/index.css',
+  'css/playlist.css',
+  'images/1.png',
+  'images/2.png',
+  'images/3.png',
+  'images/4.png',
+  'images/5.png',
+  'images/6.png',
+  '/images/backward-solid.svg',
+  '/images/blast.gif',
+  'images/forward-solid.svg',
+  'images/logo.svg',
+  'images/pause-solid.svg',
+  'images/radio.png'
+];
+
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+      // TODO 4 - Add fetched files to the cache
+
+    }).catch(error => {
+
+      // TODO 6 - Respond with custom offline page
+
+    })
+  );
+});
